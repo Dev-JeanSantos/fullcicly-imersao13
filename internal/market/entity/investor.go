57c07e06b867499a1f01f3a5dc54d9201a269220
Dev-JeanSantos <jeancbsan@gmail.com>
2023-06-20 10:22:16 -0300

    created class investor

--- internal/market/entity/investor.go
+++ internal/market/entity/investor.go
@@ -1,12 +1,49 @@
 package entity
 
 type Investor struct {
-	ID             string
-	Name           string
-	AssetPossition []*InvertorAssetPosition
+	ID            string
+	Name          string
+	AssetPosition []*InvestorAssetPosition
 }
 
-type InvertorAssetPosition struct {
+func NewInvestor(id string) *Investor {
+	return &Investor{
+		ID:            id,
+		AssetPosition: []*InvestorAssetPosition{},
+	}
+}
+
+func (i *Investor) AddAssetPosition(assetPosition *InvestorAssetPosition) {
+	i.AssetPosition = append(i.AssetPosition, assetPosition)
+}
+
+func (i *Investor) UpdateAssetPosition(assetId string, qtdShares int) {
+	assetPosition := i.GetAssetPossition(assetId)
+	if assetPosition == nil {
+		i.AssetPosition = append(i.AssetPosition, NewInvestorAssetPosstion(assetId, qtdShares))
+	} else {
+		assetPosition.Shares += qtdShares
+	}
+}
+
+func (i *Investor) GetAssetPossition(assetId string) *InvestorAssetPosition {
+
+	for _, assetPosition := range i.AssetPosition {
+		if assetPosition.AssetId == assetId {
+			return assetPosition
+		}
+	}
+	return nil
+}
+
+type InvestorAssetPosition struct {
 	AssetId string
 	Shares  int
+}
+
+func NewInvestorAssetPosstion(assetId string, shares int) *InvestorAssetPosition {
+	return &InvestorAssetPosition{
+		AssetId: assetId,
+		Shares:  shares,
+	}
 }
